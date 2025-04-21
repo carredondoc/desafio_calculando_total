f23cf5dc44213a235e88570991975d8929bcdcef
carredondoc <carlos.arredondo.calderon@gmail.com>
2025-04-21 11:14:00 -0400

    Introduccion JavaScript 01: Desafio Calculando el Total

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let precio = 400000;
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+
+document.querySelector(".cantidad").innerHTML = 0;
+document.querySelector(".valor-total").innerHTML = 0;
